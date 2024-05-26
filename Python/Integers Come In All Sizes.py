ab45f2aa92bec65b0ca1c16b937d0f3a7b47f8eb
IsaacAsante <isaac.editor16@gmail.com>
2024-05-26 23:48:05 +0800

    Solution added: Integers come in all sizes

---
+++ Python/Integers Come In All Sizes.py
@@ -0,0 +1,19 @@
+"""Author: Isaac Asante
+ * HackerRank URL for this exercise: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+ * Original video explanation: https://www.youtube.com/watch?v=_l2_bBBdbxQ
+ * Last verified on: May 26, 2024
+
+ * IMPORTANT:
+ * This code is meant to be used as a solution on HackerRank (link above).
+ * It is not meant to be executed as a standalone program.
+ */
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+# The answer
+print(a**b + c**d)
